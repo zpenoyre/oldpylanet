dc6b60a0cae54cacbb65eeee50d2a3ec9bcc86bb
Zephyr Penoyre <zephyrpenoyre@gmail.com>
2018-11-02 16:01:19 +0000

    moved to astropy units. starting to add deltas for star+companion. a few holes still in there.

--- model.py
+++ model.py
@@ -2,12 +2,17 @@ import numpy as np
 
 import scipy.optimize
 import scipy.integrate
-Gyr=3.925e8 #in Rsun, Msun and yrs
-G=2946 #in Rsun, Msun and days
-Rsun=6.96e8 #in m
-Msun=2e30 #in kg
-yr=3.15e7 #in seconds
-day=86400 #in seconds
+import astropy.units as u
+import astropy
+#Gyr=3.925e8 #in Rsun, Msun and yrs
+G=astropy.constants.G #2946 #in Rsun, Msun and days
+h=astropy.constants.h
+k=astropy.constants.k_B
+c=astropy.constants.c
+#Rsun=6.96e8 #in m
+#Msun=2e30 #in kg
+#yr=3.15e7 #in seconds
+#day=86400 #in seconds
 
 #Mj=0.000954 #in Msun
 #Me=0.000003 #in Msun
@@ -23,18 +28,18 @@ day=86400 #in seconds
 
 class system:
     def __init__(self):
-        self.M=1 # 1 Msun
-        self.Mp=0.001 # 1 Mjupiter
-        self.R=1 # 1 Rsun
-        self.Rp=0.1 # 1 Rjupiter
-        self.a=20 # ~ orbit of mercury in Rsun
-        self.e=0.01
-        self.T=5000 # Tsun
-        self.Tp=500
+        self.M=1*u.Msun # 1 Msun
+        self.Mp=0.001*u.Msun # 1 Mjupiter
+        self.R=1*u.Rsun # 1 Rsun
+        self.Rp=0.1*u.Rsun # 1 Rjupiter
+        self.T=5000*u.K # Tsun
+        self.Tp=500*u.K
     
-        self.period=1 # in days
+        self.a=20*u.Rsun # ~ orbit of mercury in Rsun
+        self.e=0.01
+        self.period=1*u.d # in days
         
-        self.D=100 # in kpc
+        self.D=100*u.kpc # in kpc
         
         self.Ag=0.2 # geometric albedo
         self.beta=1.03 # stellar response to tides for n=3 polytrope
@@ -43,7 +48,7 @@ class system:
         self.Agp=0.2 # geometric albedo of companion
         self.betap=1.03 # companion response to tides for n=3 polytrope
         
-        self.tPeri=0 # time of periapse
+        self.tPeri=0*u.d # time of periapse
         self.vPhi=0.5*np.pi # azimuthal projection angle (relative to periapse)
         self.vTheta=0.49*np.pi # polar projection angle (approx pi/2 for transiting planet)
     
@@ -51,10 +56,10 @@ class system:
     # note: could include mass here - though technically will just give M+Mp - leaving out for now   
     def find_a(self):
         period=2*np.pi*np.sqrt(self.a**3/(G*(self.M+self.Mp)))
-        self.a=self.a*(period/self.period)**(2/3)
+        self.a=(self.a*(period/self.period)**(2/3)).to(u.AU)
         return self.a
     def find_period(self):
-        self.period=2*np.pi*np.sqrt(self.a**3/(G*(self.M+self.Mp)))
+        self.period=(2*np.pi*np.sqrt(self.a**3/(G*(self.M+self.Mp)))).to(u.d)
         return self.period
     
     # General orbital parameters as a function of time
@@ -85,7 +90,7 @@ class system:
         
     # Can be used to find t (when t argument omitted) or solved to find eta (when t supplied) [raw unit->days]
     def find_t(self,eta,t=0):
-        return np.sqrt((self.a**3)/(G*(self.M+self.Mp))) * (eta - self.e*np.sin(eta)) - (t-self.tPeri)
+        return (np.sqrt((self.a**3)/(G*(self.M+self.Mp))) * (eta - self.e*np.sin(eta)) - (t-self.tPeri)).to(u.d)
     
     # Finds eta for a given time (inverting t=(T/2pi)*(eta - e sin(eta)) )
     # note: currently using approximate solution to O(e^3) from Penoyre and Sandford 2018 - see OoT for exact solution
@@ -97,4 +102,56 @@ class system:
         eta3=(self.e**3)*np.sin(eta0)*(1-(3/2)*(np.sin(eta0)**2))
         thisEta=eta0+eta1+eta2+eta3 #accurate up to and including O(e^3)
         return thisEta
+    
+    def find_epsilons(self,t): # Finds and returns the fractional radius variation for both objects
+        epsilon=find_epsilon(self,t,self.beta,self.Mp,self.M,self.R,self.vTheta,self.vPhi) # fractional radius change of star
+        epsilon_c=find_epsilon(self,t,self.betap,self.M,self.Mp,self.Rp,self.vTheta,self.vPhi+np.pi) # fractional radius change of planet
+        return epsilon,epsilon_c
+    def find_dL_t(self,t): # The luminosity change of both the star and companion due to tides
+        epsilon,epsilon_c=self.find_epsilons(t)
+        delta=-(49+16*(self.beta**-1))*epsilon/40
+        delta_c=-(49+16*(self.betap**-1))*epsilon_c/40
+        lum=1 # this is where we calculate the luminosity of the star and the companion
+        lum_c=0.001
+        return lum*delta + lum_c*delta_c
+        
+    def find_vs(self,t): # Finds and returns the line of sight velocity of both objects
+        v=find_v(self,t,self.M,self.vPhi)
+        v_c=find_v(self,t,self.Mp,self.vPhi+np.pi)
+        return v,v_c
+    def find_dL_b(self,t): # The luminosity change of both the star and companion due to beaming
+        v,v_c=self.find_v(t)
+        # need a good way of integrating over F_l over waveband of telescope
+        return
+        
+    def find_dL_r(self,t): # The reflected luminosity of both star and planet
+        Phi=self.find_Phi(t)
+        gamma=np.arccos(-np.sin(self.vTheta)*np.cos(self.vPhi-Phi))
+        gamma_c=np.arccos(-np.sin(self.vTheta)*np.cos(self.vPhi+np.pi-Phi))
+        d=self.find_d(t)
+        delta=self.Ag*np.power(self.Rp/d,2)*(np.sin(gamma)+(np.pi-gamma)*np.cos(gamma))
+        delta_c=self.Agp*np.power(self.R/d,2)*(np.sin(gamma_c)+(np.pi-gamma_c)*np.cos(gamma_c))
+        lum=1
+        lum_c=0.001
+        return lum*delta + lum_c*delta_c
+        
+# Finds the fractional radius variation at some angle (theta,phi) along the line of sight due to tides (epsilon = R/R0 - 1) for a particular body (can be star or companion)
+def find_epsilon(sys,t,beta,m,M,r,theta,phi):
+    eta=system.find_eta(t)
+    alpha=beta*(m/M)*np.power(r/sys.a,3)/4
+    gamma=alpha*np.power(1-sys.e*np.cos(eta),-3)
+    Phi=sys.find_Phi(t)
+    psi=phi-Phi
+    return gamma*2*(3 * np.sin(theta)**2 * np.cos(psi)**2 - 1)
+# Finds the line of sight velocity of an object
+def find_v(sys,t,M,vPhi):
+    # Using equation from Lovis+Fischer 2010 (I.e. not rederived by hand yet)
+    Phi=sys.find_Phi(t)
+    psi=vPhi-Phi
+    v=-np.sqrt(G/((sys.M+sys.Mp)*sys.a*(1-sys.e**2)))*M*np.sin(sys.vTheta)*(np.sin(psi)+sys.e*np.sin(vPhi))
+    # this should have sign such that it's positive when moving away from the observer
+    return v
+    
+def F_l(R,D,T,l): # dF/dl at a given wavelength
+    return 2*np.pi*np.power(R/D,2)*(h*c**2/l**5)/(np.exp(h*c/(l*k*T))-1)
     
