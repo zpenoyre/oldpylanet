ade0053cf4cf198f0e80163d57d1a7ad0e6abf6e
Zephyr Penoyre <zephyrpenoyre@gmail.com>
2018-12-11 21:06:39 +0000

    Added functions to clean gaps and rogue data points. Detrending algorithms up and running. Simple plotting tools added. Space triangles reaching dangerous levels.

--- data.py
+++ data.py
@@ -201,7 +201,8 @@ def downloadKepler(KIC, dataDirectory):
 
     #list to hold names of long cadence light curve fits files
     n_lcs = 0
-    for fileToDownload in fileNames:
+    for i,fileToDownload in enumerate(fileNames):
+        print('downloading file ',i,' of ',len(fileNames))
         targetFile = ftpfolder+fileToDownload
         #print targetFile
         lcName = dataDirectory+'/'+fileToDownload
@@ -265,3 +266,153 @@ def getData(target, dataFolderPath, cadence="both", plot=True):
     print("cadence is {0}".format(cadence))
     return keplerLC(KIC, dataDirectory, cadence)
 
+# lots of nans in kepler data, this will find them and either approximate the value or cut our losses and split the observing period up
+# nMin can be used to specify the number of data points below which we won't bother keeping the data (i.e. if it's less than a full period)
+def cleanGaps(ts,fs,es,nMin=0):
+    nPeriods=len(ts)
+    times=[]
+    fluxs=[]
+    errors=[]
+    for i in range(nPeriods):
+        theseTs=ts[i]
+        theseFs=fs[i]
+        theseEs=es[i]
+        nPoints=theseTs.size
+        if nPoints<nMin:
+            continue
+        gaps=np.flatnonzero(~np.isfinite(theseTs+theseFs+theseEs)) # if any of the three is NaN this will give there indices 
+        gaps=np.hstack([0,gaps,nPoints]) # adds in 0 and N as boundaries
+        #print('gaps: ',gaps)
+        skips=gaps[1:]-gaps[:-1] # where this equals 1 we have multiple skipped data points concurrently (i.e. a true gap)
+        #print('skips: ',skips)
+        singles=gaps[np.flatnonzero(skips>1)[1:]]
+        # where there is just one bad data point we just average the two either side
+        theseTs[singles]=0.5*(theseTs[singles+1] + theseTs[singles-1])
+        theseFs[singles]=0.5*(theseFs[singles+1] + theseFs[singles-1])
+        theseEs[singles]=0.5*(theseEs[singles+1] + theseEs[singles-1])
+        
+        jumps=np.flatnonzero((skips[1:]==1) & (skips[:-1]!=1))+1 #first nan in a long string of nans
+        endJumps=np.flatnonzero((skips[:-1]==1) & (skips[1:]!=1))+1 #last nan in a long string of nans
+        #print('jumps: ',jumps)
+        #print('endJumps: ',endJumps)
+        #print(gaps[jumps])
+        #print(gaps[endJumps])
+        if jumps.size==0: # no big jumps to cut output
+            times.append(theseTs)
+            fluxs.append(theseFs)
+            errors.append(theseEs)
+        else:
+            for j in range(jumps.size+1):
+                #print('j: ',j)
+                #print('of ',jumps.size+1)
+                if j==0:
+                    lowEnd=0
+                else:
+                    lowEnd=gaps[endJumps[j-1]]+1 # go one beyond the end of the last jumps
+                if j==(jumps.size):
+                    highEnd=nPoints
+                else:
+                    highEnd=gaps[jumps[j]] # end one before the next jump starts 
+                if (highEnd-lowEnd)<nMin:
+                    continue
+                #print('lowEnd: ',lowEnd)
+                #print('highEnd: ',highEnd)
+                #print('sum: ',np.sum(theseTs[lowEnd:highEnd]))
+                times.append(theseTs[lowEnd:highEnd])
+                fluxs.append(theseFs[lowEnd:highEnd])
+                errors.append(theseEs[lowEnd:highEnd])
+    return times,fluxs,errors
+    
+# CURRENTLY BREAKS FOR EVEN WINDOWSIZE!
+def detrend(times,fluxs,errors,period,method='box',cadence=-1):
+    nObs=len(times)
+    if cadence==-1:
+        cadence=np.median(times[1:]-times[:-1])
+    #print('cadence: ',cadence)
+    if method=='box':
+        windowWidth=int(0.5*period/cadence)
+        ts=times[windowWidth:-windowWidth]
+        fs=fluxs[windowWidth:-windowWidth]
+        es=errors[windowWidth:-windowWidth] # do we want to try and find the error in the median?
+        indices=np.array([np.arange(i,i+2*windowWidth) for i in range(nObs-2*windowWidth)])
+        fMed=np.median(fluxs[indices],axis=1)
+        eMed=np.median(np.abs(fs-fMed))
+        #print(np.max(indices))
+        #print(indices.shape)
+        #print(fs.size)
+        return ts,fs-fMed,np.sqrt(es**2 + eMed**2)
+    if method=='triangle':
+        windowSize=2*int(period/cadence) + 1
+        window=np.hstack([np.arange(int(windowSize/2)+1),np.arange(int(windowSize/2),0,-1)])
+        window=window/np.sum(window)
+    if method=='castle':
+        windowWidth=int(period/cadence)
+        ts=times[windowWidth:-windowWidth]
+        fs=fluxs[windowWidth:-windowWidth]
+        es=errors[windowWidth:-windowWidth] # do we want to try and find the error in the median?
+        
+        #setting up the weighting function
+        Ts=np.arange(-windowWidth,windowWidth+1)
+        #print('nObs: ',nObs)
+        #print('windowWidth: ',windowWidth)
+        #print('Ts.size: ',Ts.size)
+        sinc=np.sin(2*np.pi*Ts/windowWidth) / (2*np.pi*Ts/windowWidth)
+        nans=np.flatnonzero(~np.isfinite(sinc)) # should be single nan at sinc(0) - numpy can't handle this...
+        sinc[nans]=1
+        weights=sinc/((0.5*windowWidth)**2 - Ts**2)
+        weightSum=np.sum(weights)
+        weights=weights/weightSum
+        
+        indices=np.array([np.arange(i,i+2*windowWidth) for i in range(nObs-2*windowWidth)])
+        #print('indices.shape: ',indices.shape)
+        #print('max index: ',np.max(indices))
+        argsort=np.argsort(fluxs[indices],axis=1)
+        #print('argsort shape: ',argsort.shape)
+        #print('argsort: ',argsort)
+        sortWeights=weights[argsort]
+        #print('sortWeights shape: ',sortWeights.shape)
+        #print('sortWeights: ',sortWeights)
+        cumSum=np.cumsum(sortWeights,axis=1)
+        #print('cumSum shape: ',cumSum.shape)
+        #print('cumSum: ',cumSum)
+        medians=np.argmin(np.abs(cumSum-0.5),axis=1)
+        #print('medians shape: ',medians.shape)
+        #print('medians: ',medians)
+        medianIndices=indices[np.arange(medians.size),argsort[np.arange(medians.size),medians]]
+        #print('medianIndices: ',medianIndices)
+        cosTerm=fluxs[medianIndices]
+        #print(0.5*period/cadence)
+        halfWidth=int(0.5*(period/cadence))
+        #print('halfWidth: ',halfWidth)
+        sinTerm=fs-0.5*(fluxs[halfWidth:-windowWidth-halfWidth-1]+fluxs[windowWidth+halfWidth:-halfWidth-1])
+        fMed=cosTerm+sinTerm
+        #print('fs.size: ',fluxs[0:-2*windowWidth].size)
+        #print('fluxs[medianIndices].size: ',fluxs[2*windowWidth:].size)
+        eMed=np.median(np.abs(fs-fMed))
+        return ts,fs-fMed,np.sqrt(es**2 + eMed**2)
+        
+
+def cleanData(ts,fs,es,period,nMin=1000,cadence=-1,detrendMethod='box'):
+    times,fluxs,errors=cleanGaps(ts,fs,es,nMin=nMin)
+    nPeriods=len(times)
+    allTs=np.array([])
+    allFs=np.array([])
+    allEs=np.array([])
+    for i in range(nPeriods):
+        pTs,pFs,pEs=detrend(times[i],fluxs[i],errors[i],period,method=detrendMethod,cadence=cadence)
+        allTs=np.hstack([allTs,pTs])
+        allFs=np.hstack([allFs,pFs])
+        allEs=np.hstack([allEs,pEs])
+    return allTs,allFs,allEs
+    
+def stackData(ts,fs,es,period,nTs=100,offset=0):
+    dt=period/(nTs+1)
+    binTs=np.arange(dt/2,period-dt/2,dt+1e-6)
+    binFs=np.zeros(nTs)
+    binEs=np.zeros(nTs)
+    for i in range(nTs):
+        inBin=np.flatnonzero(np.abs(((ts-offset)%period)-binTs[i])<dt/2)
+        binFs[i]=np.median(fs[inBin])
+        binEs[i]=1.4826*np.median(np.abs(fs[inBin]-binFs[i]))/np.sqrt(inBin.size)
+    return binTs,binFs,binEs
+    
